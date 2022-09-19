dba9e81e60c4bd6271f8dadda83d9bef276afba2
Wilson Syring <wilson.syring@gmail.com>
2022-09-19 11:47:33 -0500

    made changes on the comments to remove the unsightly sqiggles

--- js/while.js
+++ js/while.js
@@ -24,7 +24,7 @@ while(num <= 32768){
 //An ice cream seller can't go home until she sells all of her cones.
 //First write enough code that generates a random number between 50 and 100
 //representing the amount of cones to sell before you start your loop.
-//Inside of the loop your code should generate another random number
+//Inside the loop your code should generate another random number
 //between 1 and 5, simulating the amount of cones being bought
 //by her clients. Use a do-while loop to log to the
 //console the amount of cones sold to each person.
