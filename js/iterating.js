21758792a268a413e89a4baee6e9b72b9df84604
Wilson Syring <wilson.syring@gmail.com>
2022-09-19 12:26:47 -0500

    almost finished with iterating arrays exercise breaking for lunch

--- js/iterating.js
+++ js/iterating.js
@@ -13,29 +13,35 @@
      * Create a log statement that will log the number of elements in the names
      * array.
      */
-    console.log(names.length);
+    console.log("There are "+names.length+" names in this array");
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
-    console.log(names[0]);
-    console.log(namess[1]);
-    console.log(namess[2]);
-    console.log(namess[3]);
+    console.log("This is the first name: "+names[0]);
+    console.log("This is the second name: "+names[1]);
+    console.log("This is the third name: "+names[2]);
+    console.log("This is the fourth name: "+names[3]);
 
     /**
      * TODO:
      * Write some code that uses a for loop to log every item in the names
      * array.
      */
+    for (let i = 0; i < names.length; i++) {
+        console.log(`This is a name of one of my instructors ${names[i]}!`);
+    }
 
 
     /**
      * TODO:
      * Refactor your above code to use a `forEach` loop
      */
+    names.forEach(function(name) {
+        console.log('Here is an Epic name: ' + name + '.');
+    });
 
     /**
      * TODO:
@@ -50,5 +56,6 @@
      *  > second([1, 2, 3, 4, 5]) // returns 2
      *  > last([1, 2, 3, 4, 5]) // return 5
      */
+
 
 })();
