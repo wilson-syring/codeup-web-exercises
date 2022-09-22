6fde602aae5ba80770167fac1f3b3b5ff83cce0b
Wilson Syring <wilson.syring@gmail.com>
2022-09-22 10:34:19 -0500

    cleaned up the code and added comments with the instructions for warmups for thorsday sept 22 2022

--- js/warmup.js
+++ js/warmup.js
@@ -1,34 +1,27 @@
 'use strict'
+// Create a function to sort the array from lowest to highest. Make sure the function accepts an array and returns an array.
 let numArr = [5,8,2,6,9,3,2];
 function sortArray(array) {
     console.log(array.sort(function(a, b){return a - b})) ;
 }
 sortArray(numArr);
+
+//Write a function that returns the reading status of each of the following books:
 let library = [
-    {
-        author: 'Bill Gates',
-        title: 'The Road Ahead',
-        readingStatus: true
+    {author: 'Bill Gates', title: 'The Road Ahead', readingStatus: true
     },
-    {
-        author: 'Steve Jobs',
-        title: 'Walter Isaacson',
-        readingStatus: true
+    {author: 'Steve Jobs', title: 'Walter Isaacson', readingStatus: true
     },
-    {
-        author: 'Suzanne Collins',
-        title:  'Mockingjay: The Final Book of The Hunger Games',
-        readingStatus: false
+    {author: 'Suzanne Collins', title:  'Mockingjay: The Final Book of The Hunger Games', readingStatus: false
     }];
+
 function readBooks (array){
-    for (let i = 0; i < array.length; i++)
-    {
+    for (let i = 0; i < array.length; i++) {
         let book = `${array[i].title} by ${array[i].author}.`;
         if (array[i].readingStatus) {
-            console.log("Already read: " + book);
-        } else
-        {
-            console.log("You still need to read: " + book);
+            console.log("I have already read this book: " + book);
+        } else {
+            console.log("I still need to read this book: " + book);
         }
     }
 }
