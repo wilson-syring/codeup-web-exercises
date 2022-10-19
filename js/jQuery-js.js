fd3566291c8c4921c8ce18f5ecd19b64cc9f778e
Wilson Syring <wilson.syring@gmail.com>
2022-10-19 16:25:03 -0500

    completed jquery selectors exercise

--- js/jQuery-js.js
+++ js/jQuery-js.js
@@ -2,6 +2,24 @@
 window.onload = function() {
     alert( 'The page has finished loading!' );
 }
+
 $(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+var contents = $('#head').html();
+alert(contents);
+
+var contents2 = $('#headText').html();
+alert(contents2);
+
+$('.codeup').css('border', '1px solid #F00');
+
+$('li').css('font-size', '20px');
+
+$('h1,p,li').hover(
+    function() {
+        $(this).css('background-color', '#63b222');
+    },
+
+);
